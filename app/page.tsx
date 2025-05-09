7ea281a262bd2c9bf9e1b5c161f2fd27f1366dfc
kan <info@shepherd-ai.com>
2025-05-10 01:24:52 +0900

    :wrench:test

--- app/page.tsx
+++ app/page.tsx
@@ -9,7 +9,7 @@ export default function Home() {
 
   return (
     <div className="container mx-auto px-4 py-8">
-      <h1 className="text-3xl font-bold text-center mb-8">音声分析アプリ</h1>
+      <h1 className="text-3xl font-bold text-center mb-8">音声分析アプリ２</h1>
       
       <div className="max-w-2xl mx-auto">
         {!jobId ? (
